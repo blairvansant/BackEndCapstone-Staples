e7ec2b412121b40b5fc1ee3d0c984bc36e772382
blairvansant <blairbot@gmail.com>
2017-06-09 14:51:30 -0500

    add single item model

---
+++ Staples/Models/SingleItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Staples.Models
+{
+    public class SingleItemModel
+    {
+        [Key]
+        public int id { get; set; }
+        public string DateOfPurchase { get; set; }
+        public string ExpirationDate { get; set; }
+        public string TimeToPurchase { get; set; }
+    }
+}
